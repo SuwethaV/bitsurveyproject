0adbc39ff1ca96d9790415ab1955e2c695081018
Suwetha V <swethanila1573@gmail.com>
2025-03-03 20:14:27 +0530

    changes made to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DashboardLayout from "./pages/dashboardnull";
+import SurveyCreation from "./pages/createsurvey";
+import QuestionModal from "./pages/QuestionModal";
+import DashboardCreated from "./pages/dashboard";
+import AssetsDict from "./pages/assets";
+import SurveyTemplate from "./pages/assettemplate";
+import FeedbackForm from "./pages/Feedbackform";
+import AcademicFeedbackForm from "./pages/academicfb";
+import EditableTable from "./pages/producttemplate";
+import Login from "./pages/login/login";
+import Register from "./pages/register/register";
+import GroupCreationModal from "./pages/groupcreation";
+import Card from "./pages/card";
+import Sample from "./pages/card";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <BrowserRouter>
+    <Routes>
+      {/* Public Routes */}
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/user" element={<DashboardUser/>} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/mentor" element={<Mentor />} />
+      <Route path="/survey" element={<MySurvey />} />
+      <Route path="/dashboard" element={<DashboardCreated />} />
+      <Route path="/dashboardnull" element={<DashboardLayout />} />
+      <Route path="/templates" element={<AssetsDict />} />
+      <Route path="/create-survey" element={<SurveyCreation />} />
+      <Route path="/create-new" element={<SurveyCreation />} />
+      <Route path="/question-modal" element={<QuestionModal />} />
+      <Route path="/question-modal" element={<QuestionModal />} />
+      <Route path="/Feedbackform" element={<FeedbackForm />} />
+      <Route path="/academicfb" element={<AcademicFeedbackForm />} />
+      <Route path="/producttemplate" element={<EditableTable />} />
+    </Routes>
+  </BrowserRouter>
+    
+)};
 
-export default App
+export default App;
+
